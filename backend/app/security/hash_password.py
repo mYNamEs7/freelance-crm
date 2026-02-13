c3e4ac53863d3be88008a4c36a8f9026af587fc2
mYNamEs7 <yura97078@gmail.com>
2026-02-13 21:28:28 +0300

    fix pwd hash

--- backend/app/security/hash_password.py
+++ backend/app/security/hash_password.py
@@ -3,7 +3,9 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_pwd(password: str) -> str:
-    return pwd_context.hash(password)
+    password_bytes = password.encode("utf-8")
+    return pwd_context.hash(password_bytes)
 
 def verify_pwd(password: str, password_hash: str) -> bool:
-    return pwd_context.verify(password, password_hash)
+    password_bytes = password.encode("utf-8")
+    return pwd_context.verify(password_bytes, password_hash)
